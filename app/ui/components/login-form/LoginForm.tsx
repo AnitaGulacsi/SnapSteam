7bf1533073d4a4b6708cf050bd237c8865bb27b4
Anita Gulacsi <gulacsi_anita@yahoo.com>
2023-12-14 14:36:26 +0200

    Enhance input item alignment with flex

--- app/ui/components/login-form/LoginForm.tsx
+++ app/ui/components/login-form/LoginForm.tsx
@@ -2,17 +2,21 @@ import { Button } from "../buttons/button";
 
 export const LogInForm = () => (
   <form className="hidden lg:block bg-green p-10 rounded-3xl">
-    <div className="flex flex-col justify-center">
-      <input
-        className="p-2 rounded-full border border-teal-400 bg-transparent text-teal-100 outline-none focus:outline-none"
-        type="text"
-        placeholder="Username"
-      />
-      <input
-        className="mt-5 p-2 rounded-full border border-teal-400 bg-transparent text-teal-100 outline-none focus:outline-none"
-        type="password"
-        placeholder="Password"
-      />
+    <div className="flex flex-col">
+      <div className="flex justify-center">
+        <input
+          className="p-2 w-64 rounded-full border border-teal-400 bg-transparent text-teal-100 outline-none focus:outline-none"
+          type="text"
+          placeholder="Username"
+        />
+      </div>
+      <div className="flex justify-center">
+        <input
+          className="mt-5 p-2 w-64 rounded-full border border-teal-400 bg-transparent text-teal-100 outline-none focus:outline-none"
+          type="password"
+          placeholder="Password"
+        />
+      </div>
       <span className="flex justify-center mt-10">
         <Button type="form">LogIn</Button>
       </span>
