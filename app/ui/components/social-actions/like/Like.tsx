66ddee647f90842740ae02a460beb6ed20c8e27b
Anita Gulacsi <gulacsi_anita@yahoo.com>
2024-01-15 12:05:55 +0200

    add like counter

--- app/ui/components/social-actions/like/Like.tsx
+++ app/ui/components/social-actions/like/Like.tsx
@@ -1,9 +1,19 @@
+"use client";
 import Image from "next/image";
 import like from "@/public/icon/like.svg";
+import { useState } from "react";
 
-export const Like = () => (
-  <div className="flex flex-row items-center gap-2">
-    <Image src={like} alt={"like"} />
-    <p>0</p>
-  </div>
-);
+export const Like = () => {
+  const [isLiked, setIsLiked] = useState(0);
+
+  const handleLiked = () => {
+    setIsLiked(isLiked + 1);
+  };
+
+  return (
+    <div className="flex flex-row items-center gap-2">
+      <Image onClick={handleLiked} src={like} alt={"like"} />
+      <p>{isLiked}</p>
+    </div>
+  );
+};
