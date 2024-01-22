409356c9f65d575511efb08578fe0536dc4a6033
Anita Gulacsi <gulacsi_anita@yahoo.com>
2024-01-22 15:40:53 +0200

    add Metrics component

---
+++ app/ui/components/profile-page/metrics/Metrics.tsx
@@ -0,0 +1,45 @@
+import { FC } from "react";
+
+interface MetricsProps {
+  post: string;
+  postCounter: number;
+  followers: string;
+  followersCounter: number;
+  following: string;
+  followingCounter: number;
+}
+
+export const Metrics: FC<MetricsProps> = ({
+  post,
+  postCounter,
+  followers,
+  followersCounter,
+  following,
+  followingCounter,
+}) => {
+  return (
+    <div className="bg-white flex flex-row gap-10 justify-between p-10 sm:px-24 md:justify-center">
+      <div className="flex flex-col">
+        <div className="h-[100px] w-[100px] bg-red-800 rounded-full" />
+        <div className="flex flex-row gap-2 ml-5 xl:hidden">
+          <p>{post}</p>
+          <p>{postCounter}</p>
+        </div>
+      </div>
+      <div className="flex flex-col gap-5 xl:flex-row">
+        <div className="flex flex-row gap-2 ml-5 hidden xl:block">
+          <p>{post}</p>
+          <p>{postCounter}</p>
+        </div>
+        <div className="text-center">
+          <p>{followers}</p>
+          <p>{followersCounter}</p>
+        </div>
+        <div className="text-center">
+          <p>{following}</p>
+          <p>{followingCounter}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
--- app/username/page.tsx
+++ app/username/page.tsx
@@ -1,9 +1,20 @@
 import { Navbar } from "../ui/components/navbar/Navbar";
+import { Metrics } from "../ui/components/profile-page/metrics/Metrics";
 
 export default function Page() {
   return (
-    <main>
+    <main className="bg-green h-screen">
       <Navbar welcome={"Welcome"} username={"@username"} />
+      <div className="flex justify-center">
+        <Metrics
+          post={"Post:"}
+          postCounter={0}
+          followers={"Followers"}
+          followersCounter={0}
+          following={"Following"}
+          followingCounter={0}
+        />
+      </div>
     </main>
   );
 }
