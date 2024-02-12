c24792a31885212a2d6f61074d83ce7f1950abf9
Anita Gulacsi <gulacsi_anita@yahoo.com>
2024-02-12 10:21:45 +0200

    Refactor: Rename 'signin' to 'RegistrationForm' for clarity and consistency

--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -1,10 +1,10 @@
-import { SigninForm } from "../ui/components/forms/SigninForm";
+import { RegistrationForm } from "../ui/components/forms/RegistrationForm";
 
 export default function Page() {
   return (
     <main className="bg-green flex items-center justify-center h-screen">
       <div className="border bg-white">
-        <SigninForm />
+        <RegistrationForm />
       </div>
     </main>
   );
